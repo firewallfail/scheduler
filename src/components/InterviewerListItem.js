05775a3d600f3a8061a8657c082c321e32731210
firewallfail <firewall.fail@hotmail.com>
2022-05-18 09:46:07 -0400

    Implement InterviewerListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import classNames from "classnames";
+
+import "components/InterviewerListItem.scss"
+
+export default function InterviewerListItem(props) {
+  const { id, name, avatar, selected, setInterviewer } = props;
+
+  const interviewerListItemClass = classNames("interviewers__item", {
+    "interviewers__item--selected": selected
+  });
+
+  return (
+    <li className={interviewerListItemClass} onClick={() => setInterviewer(id)}>
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      {name}
+    </li>
+  );
+}
