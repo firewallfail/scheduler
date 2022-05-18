cab4ad1fa33fcc61fc4d2e94987625c3999863b2
firewallfail <firewall.fail@hotmail.com>
2022-05-18 16:19:12 -0400

    Implement most of Appointment functionality

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,14 +5,15 @@ import "./InterviewerList.scss"
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
+  const { value, onChange } = props;
 
   const interviewers = props.interviewers.map(interviewer =>
     <InterviewerListItem
       key={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      selected={interviewer.id === props.interviewer}
-      setInterviewer={() => props.setInterviewer(interviewer.id)}
+      selected={interviewer.id === value}
+      setInterviewer={() => onChange(interviewer.id)}
     />);
 
   return (
