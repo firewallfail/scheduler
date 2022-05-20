344a838c1236bbfe598ed85771d35446646ac194
firewallfail <firewall.fail@hotmail.com>
2022-05-19 20:17:50 -0400

    Forgot to commit

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,5 @@
-export function getAppointmentsForDay(state, day) {
+export default function getAppointmentsForDay(state, day) {
   const appointmentDay = state.days.filter(x => x.name === day);
-
   if (appointmentDay.length === 0) return appointmentDay;
-
   return appointmentDay[0].appointments.map(x => state.appointments[x]);
 }
