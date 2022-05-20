e52442e14e755b291244992de1196734f0faabff
firewallfail <firewall.fail@hotmail.com>
2022-05-20 09:25:20 -0400

    Improve guard clause for .length

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,6 @@
 export function getAppointmentsForDay(state, day) {
   const appointmentDay = state.days.filter(x => x.name === day);
-  if (appointmentDay.length === 0) return appointmentDay;
+  if (!appointmentDay.length) return appointmentDay;
   return appointmentDay[0].appointments.map(x => state.appointments[x]);
 }
 
