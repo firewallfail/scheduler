d948c41a7d65c3223b75add8016d1007813e219c
firewallfail <firewall.fail@hotmail.com>
2022-05-19 21:10:13 -0400

    Improve readability in useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -38,9 +38,15 @@ export default function Application() {
       axios.get("api/days"),
       axios.get("api/appointments"),
       axios.get("api/interviewers")
-    ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    })
+    ])
+      .then((all) => {
+        setState(prev => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data
+        }));
+      })
       .catch((err) => console.log(err));
   }, []);
 
