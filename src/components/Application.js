10a85eedf8fd0027e0e294da85dc055775924b93
firewallfail <firewall.fail@hotmail.com>
2022-05-19 21:08:38 -0400

    Add a catch for the Promise.all

--- src/components/Application.js
+++ src/components/Application.js
@@ -41,6 +41,7 @@ export default function Application() {
     ]).then((all) => {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     })
+      .catch((err) => console.log(err));
   }, []);
 
   return (
