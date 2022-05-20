3217d5b2196690985e4677525b6dd324688df6c3
firewallfail <firewall.fail@hotmail.com>
2022-05-19 20:51:16 -0400

    Add interview information to <Appointment/>

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import getAppointmentsForDay from "../helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 
 import "components/Application.scss";
 
@@ -12,19 +12,34 @@ export default function Application() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   const setDay = day => setState({ ...state, day });
 
   useEffect(() => {
     Promise.all([
       axios.get("api/days"),
-      axios.get("api/appointments")
+      axios.get("api/appointments"),
+      axios.get("api/interviewers")
     ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }))
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     })
   }, []);
 
@@ -51,11 +66,7 @@ export default function Application() {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments.map(appointment =>
-          <Appointment
-            key={appointment.id}
-            {...appointment}
-          />)}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,13 @@
-export default function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, day) {
   const appointmentDay = state.days.filter(x => x.name === day);
   if (appointmentDay.length === 0) return appointmentDay;
   return appointmentDay[0].appointments.map(x => state.appointments[x]);
+}
+
+export function getInterview(state, interview) {
+  if (!interview) return null;
+  return {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  };
 }
